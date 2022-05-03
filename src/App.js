cba5e9ab3894bf1bcc325bfeb90751fc40e5bb98
dragonyu1010 <provic0099@gmail.com>
2022-05-03 12:00:01 +0800

    listening on change of metamask and add user information on signin component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import NavBar from "./components/NavBar";
 import Footer from "./components/footer/Footer";
 import { WalletContext, WalletContextProps } from "./context/walletContext";
+import { useEffect } from "react";
 function App() {
+	useEffect(() => {
+		if(window.ethereum) {
+			window.ethereum.on('chainChanged', () => {
+				window.location.reload();
+			})
 
+			window.ethereum.on('accountsChanged', () => {
+				window.location.reload();
+			})
+		}
+	})
 	return (
 		<WalletContext.Provider value={WalletContextProps()}>
 			<NavBar/>
--- src/components/NavBar/SignIn.js
+++ src/components/NavBar/SignIn.js
@@ -1,10 +1,36 @@
+import { useState, useContext, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { WalletContext } from "../../context/walletContext";
 
 function SignIn() {
+    const [name, setName] = useState(null)
+    const [avatar, setAvatar] = useState(null)
+    const [walAddr, setWalAddr] = useState(null)
+
+    const walletContext = useContext(WalletContext)
+    const getUserInfo = () => {
+        fetch( process.env.REACT_APP_API_BASE_URL + 'user/' +  walAddr)
+            .then(res => res.json())
+            .then( data => {
+                setName(data[0].name)
+                setAvatar(data[0].avatar)
+            })
+    }
+    useEffect(() => {
+        if(walletContext.wallet !== null) {
+            getUserInfo()
+            setWalAddr(walletContext.wallet)
+        }
+    }, [walletContext])
     return (
         <div className="signIn">
             <Link to="/create"><button className='normal'>Create</button></Link>
-            <Link to="/signin"><button className='normal'>Sign In</button></Link>
+            {
+                walletContext.wallet !== null ?
+                <Link to="/signin"><button className='normal'>{walletContext.wallet}</button></Link>
+                :
+                <Link to="/signin"><button className='normal'>Sign In</button></Link>
+            }
         </div>
     )
 }
