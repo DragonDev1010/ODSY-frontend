36c192f0bb6fa3d142858c07189223e2b862f8d3
dragonyu1010 <provic0099@gmail.com>
2022-05-03 17:27:43 +0800

    clear all local storages when browser is closed

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Footer from "./components/footer/Footer";
 import { WalletContext, WalletContextProps } from "./context/walletContext";
 import { useEffect } from "react";
 function App() {
+	// clear all local storage when browser is closed
+	localStorage.removeItem('connectedWalletAddress');
+	localStorage.removeItem('userName');
 	useEffect(() => {
 		if(window.ethereum) {
 			window.ethereum.on('chainChanged', () => {
