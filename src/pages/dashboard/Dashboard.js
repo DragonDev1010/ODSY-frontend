a4f43ab58188a3d48a6170df6d11f553b275201c
dragonyu1010 <provic0099@gmail.com>
2022-05-18 03:10:44 +0800

    add authorization for dashboard page

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,5 +1,5 @@
 import { useState, useEffect, useContext } from 'react'
-
+import {useNavigate} from 'react-router-dom'
 import {WalletContext} from '../../context/walletContext'
 import {MessageContext} from '../../context/messageContext'
 import MetamaskConnect from '../../actions/metamaskConnect'
@@ -9,12 +9,9 @@ import Nfts from './Nfts'
 import CollectList from './CollectList'
 
 function Dashboard() {
+    let navigate = useNavigate();
     const walContext = useContext(WalletContext)
-    useEffect( async () => {
-        if(walContext.wallet === null) {
-            await MetamaskConnect()
-        }
-    }, [])
+    
     const [data, setData] = useState(null)
     const [status, setStatus] = useState([])
     const [currency, setCurrency] = useState(null)
@@ -22,8 +19,10 @@ function Dashboard() {
     const [maxPrice, setMaxPrice] = useState(null)
     const [collects, setCollects] = useState([])
     const [chains, setChains] = useState([])
-
     const [panelSwitcher, setPanelSwitcher] = useState(true)
+    const [admins, setAdmins] = useState(null)
+    const [isAdmin, setIsAdmin] = useState(false)
+
     const getNftData = () => {
         let query = {}
         if(status.length > 0) 
@@ -53,36 +52,77 @@ function Dashboard() {
             .then(res => res.json())
             .then(res => setData(res))
     }
+
+    useEffect( async () => {
+        fetch(
+            process.env.REACT_APP_API_BASE_URL + 'users?admin=true',
+            {method:'GET'}
+        )
+            .then(res => res.json())
+            .then(res => {
+                if(res.length > 0) {
+                    let temp = []
+                    res.map(item => {
+                        temp.push(item.wallet.toLowerCase())
+                    })
+                    setAdmins(temp)
+                }
+            })
+        if(walContext.wallet === null) {
+            await MetamaskConnect()
+        }
+    }, [])
+    
     useEffect(() => {
         getNftData()
     }, [status, collects, chains, currency, minPrice, maxPrice, chains])
+
+    useEffect(() => {
+        if(walContext.wallet !== null && admins !== null) {
+            if(admins.includes(walContext.wallet.toLowerCase()))
+                setIsAdmin(true)
+            else {
+                setIsAdmin(false)
+                console.log('redirect')
+                return navigate("/");
+            }
+        }
+    }, [admins, walContext])
     return (
         <>
-            <div style={{paddingLeft: '50px'}}>
-                <button className='normal' onClick={() => setPanelSwitcher(!panelSwitcher)}>
-                    {panelSwitcher ? "Show Collect List" : "Show NFT List"}
-                </button>
-            </div>
-            <div className='adminDashboard'>
-                {
-                    panelSwitcher ?
-                    <>
-                        <Filter 
-                            setStatus={setStatus}
-                            setCollects={setCollects}
-                            setChains={setChains}
-                            setCurrency={setCurrency}
-                            setMinPrice={setMinPrice}
-                            setMaxPrice={setMaxPrice}
-                            />
-                        <Nfts data={data} getNftData={getNftData}/>
-                    </>
-                    :
-                    <>
-                        <CollectList/>
-                    </>
-                }
-            </div>
+        {
+            isAdmin ?
+            <>
+                <div style={{paddingLeft: '50px'}}>
+                    <button className='normal' onClick={() => setPanelSwitcher(!panelSwitcher)}>
+                        {panelSwitcher ? "Show Collect List" : "Show NFT List"}
+                    </button>
+                </div>
+                <div className='adminDashboard'>
+                    {
+                        panelSwitcher ?
+                        <>
+                            <Filter 
+                                setStatus={setStatus}
+                                setCollects={setCollects}
+                                setChains={setChains}
+                                setCurrency={setCurrency}
+                                setMinPrice={setMinPrice}
+                                setMaxPrice={setMaxPrice}
+                                />
+                            <Nfts data={data} getNftData={getNftData}/>
+                        </>
+                        :
+                        <>
+                            <CollectList/>
+                        </>
+                    }
+                </div>
+            </>
+            :
+            ""
+        }
+            
         </>
     )
 }
